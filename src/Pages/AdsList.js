42fcc228b8f3d1048a6e19eed44183bb5dc325c7
yavilarobledo <143647435+yavilarobledo@users.noreply.github.com>
2023-12-12 15:32:47 -0500

    Updated AdsList.js - ads are in cards now!

--- src/Pages/AdsList.js
+++ src/Pages/AdsList.js
@@ -1,14 +1,41 @@
+import moment from "moment";
 const AdsList = ({ ads }) => {
     return (
       <div className="ads-list">
+        
+        <div className="card-group ">
         {ads.map(ad => (
-          <div className="ad-preview" key={ad.UserId} >
-            <h2>{ ad.Desc}</h2>
-            <p>Written by { ad.ServiceRequest }</p>
-          </div>
+          
+          
+            <div className="card text-center shadow m-2" key={ad.UserId} >
+
+              <div className="card-body text-dark">
+              
+              
+                      <h4 className="card-title">{ad.ServiceRequest}</h4>
+
+                      <p className="card-text text-secondary">
+                          {ad.Desc}
+                      </p>
+
+                      <p className="card-text text-secondary">
+                      {moment(ad.DateNeeded).format('MMM DD, YYYY')}
+
+                      </p>
+                      <p className="card-text text-secondary">
+                        {ad.ZipCode}
+                      </p>
+
+              </div>
+            </div>
+          
+          
         ))}
+        </div>
       </div>
     );
   }
    
   export default AdsList;
+
+
